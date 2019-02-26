049fb76b8ce6b8396a8f9e229612d641085f0d1d
Maycon Ritzmann <maycon@mayconritzmann.com>
2019-02-26 11:28:00 -0300

    first push

---
+++ README.md
@@ -0,0 +1,9 @@
+# zabbix-zimbra-monitoring
+Monitoring Zimbra services states with zabbix.
+
+#### Files in repository:
+1) Zimbra_Services_template.xml - template for zimbra monitoring. Import on zabbix server.
+
+2) userparameter_zimbra.conf - Config with UserParameter settings. Put it in the /etc/zabbix/zabbix_agentd.d/ directory (or something like this), then include in zabbix agent configuration file ("Include=/etc/zabbix/zabbix_agentd.d/").
+
+4) zimbra_status.sh - monitoring script. Put in zabbix monitoing scripts directory (in my case its "/etc/zabbix/scripts")
---
+++ zimbra_status.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+tail=/usr/bin/tail
+
+if [[ -z "$1" ]]; then
+  exit 1
+fi
+
+zimbra_log_file="/var/log/zimbra-stats.log"
+zimbra_discovery="$(tail -n 1000 /var/log/zimbra-stats.log | grep STATUS | cut -d ':' -f 10 | sort | uniq)"
+
+case "$1" in
+  discovery)
+    declare -a services
+    n=0
+    for i in $zimbra_discovery ; do
+      services[$n]="$i"
+      let "n= $n + 1"
+    done
+
+    length=${#services[@]}
+    echo "{"
+    echo -e "\"data\":[\n"
+    for (( i=0; i<${length}; i++ ))
+    do
+      if [ $(( $i + 1 )) -lt $length ]; then
+        echo -e "\t{ \"{#ZIMBRASERVICE}\":\"${services[$i]}\" },"
+      else
+        echo -e "\t{ \"{#ZIMBRASERVICE}\":\"${services[$i]}\" }"
+      fi
+    done
+    echo -e "\n\t]\n"
+    echo "}"
+  exit 0;
+  ;;
+  *)
+
+  if [ $1 = "" ]; then
+    echo "ERROR: argument required"
+    exit 1
+  fi
+
+  state="$(tail -n 1000 $zimbra_log_file | grep STATUS | grep $1 | tail -1 | awk {'print $12'})"
+
+  if [ "$state" == "Running" ]; then
+    echo 0
+  else
+    echo 1
+  fi
+  ;;
+esac
+
+exit 0;
